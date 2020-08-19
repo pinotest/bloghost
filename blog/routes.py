3481599d937de04445c54c9d187d8b557ddf4f94
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-08-19 09:22:33 +0200

    Add funcionality add and edit post from web

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,43 @@
-from flask import render_template
+from flask import render_template, request
+from blog.forms import EntryForm
+from blog.models import Entry, db
 from blog import app
 
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    all_posts = Entry.query.filter_by(
+        is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           form.populate_obj(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
