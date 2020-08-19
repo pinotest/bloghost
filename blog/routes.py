d4d0d984d77f3d1303e20f25478aef9d37a851b7
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-08-19 12:26:30 +0200

    Change routes to one function for create and edit post"

--- blog/routes.py
+++ blog/routes.py
@@ -13,31 +13,30 @@ def index():
 
 @app.route("/new-post/", methods=["GET", "POST"])
 def create_entry():
-    form = EntryForm()
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-        else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
+    return generate_post()
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-   entry = Entry.query.filter_by(id=entry_id).first_or_404()
-   form = EntryForm(obj=entry)
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           form.populate_obj(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+    return generate_post(entry_id)
+
+
+def generate_post(entry_id=None):
+    if entry_id:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if entry_id:
+                form.populate_obj(entry)
+            else:
+                entry = Entry(title=form.title.data, body=form.body.data,
+                              is_published=form.is_published.data)
+                db.session.add(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
